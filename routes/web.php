e64b4b713d351cbbd88b28e9da6e4197648b7bd4
truongkien0815 <truongkien0815@gmail.com>
2022-11-19 23:43:54 +0700

    chuc năng mua hàng,quan ly don hang.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\DetailsController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProtypesController;
+use App\Http\Controllers\OthersController;
+use App\Http\Controllers\PaymentsController;
+use App\Http\Controllers\ManufacturersController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +19,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+ 
+require __DIR__ . '/auth.php';   
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    // Admin
+    Route::get('/admin', [MyController::class, 'admin']);
+    Route::resource('/product', ProductsController::class);
+    Route::resource('/user', UserController::class);
+    Route::resource('/manufacturer', ManufacturersController::class);
+    Route::resource('/protype', ProtypesController::class);
+    Route::resource('/payment', PaymentsController::class);
+    Route::resource('/detail', DetailsController::class);
+    Route::resource('/other', OthersController::class);
 });
+Route::get('/payment_del/{id?}', [PaymentsController::class,'delete']);
+Route::post('/xacnhan/{name?}', [DetailsController::class,'update_hoanthanh']);
+Route::get('/all_order', [DetailsController::class,'index_all']);
+Route::get('/detailhoanthanh', [DetailsController::class,'index_hoanhthanh']);
+
+Route::get('/mail', [MyController::class, 'mail']); 
+Route::get('/createpayment', [MyController::class, 'createpayment']); 
+Route::get('/payments', [MyController::class, 'payments']); 
+Route::get('/sort/{option}/{key?}', [MyController::class, 'sort']); 
+Route::get('/searchoption/{option}/{key?}', [MyController::class, 'searchoption']); 
+Route::get('/search', [MyController::class, 'search']); 
+Route::get('/others/clearcompare', [MyController::class, 'clearcompare']);
+Route::get('/others/{name}/{product_id}/{user_id}/{option?}/{key?}', [MyController::class, 'others'])->name('others');
+Route::get('/carts/{action?}/{product_id?}', [MyController::class, 'carts'])->name('carts');
+Route::get('/star/{manu_id}/{product_id}/{user_id}', [MyController::class, 'star']);
+Route::get('/products/{product_id}/{manu_id}', [MyController::class, 'products'])->name('products');
+Route::get('/{name?}', [MyController::class, 'index'])->name('index');
