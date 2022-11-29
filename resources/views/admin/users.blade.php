f41412a4911fe104965363678845a8ee593d6953
dkjbij <nhatphivo659@gmail.com>
2022-11-29 07:37:03 +0700

    lan9

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Manufactures</h1>
+          <h1>Users</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -47,11 +47,20 @@
         <table class="table table-striped projects">
           <thead>
             <tr>
-              <th style="width: 25%" class="text-center">
-                Manu id
+              <th style="width: 20%" class="text-center">
+                User id
               </th>
-              <th style="width: 25%" class="text-center">
-                Manu name
+              <th style="width: 20%" class="text-center">
+                User name
+              </th>
+              <th style="width: 20%" class="text-center">
+                Email
+              </th>
+              <th style="width: 20%" class="text-center">
+                Password
+              </th>
+              <th style="width: 20%" class="text-center">
+                Created at
               </th>
               <th style="width: 50%" class="text-center">
                 Action
@@ -59,16 +68,26 @@
             </tr>
           </thead>
           <tbody>
-            @foreach($allmanus as $value)
+            @foreach($allusers as $value)
             <tr>
               <td class="project-state">
-                {{ $value->manu_id }}
+                {{ $value->id }}
               </td>
               <td class="project-state">
-                {{ $value->manu_name }}
+                {{ $value->name }}
+              </td>
+              <td class="project-state">
+                {{ $value->email }}
+              </td>
+              <td class="project-state">
+                {{ $value->password }}
+              </td>
+              <td class="project-state">
+                {{ $value->created_at }}
               </td>
               <td class="project-actions text-center">
-                <form action="{{ url('/manufacturer/'.$value->manu_id.'/edit') }}" method="get" style="padding-bottom: 20px;">
+                <!-- <form action="{{ url('/manufacturer/'.$value->manu_id.'/edit') }}" method="get" style="padding-bottom: 20px;"> -->
+                <form action="{{ url('user/'.$value->id) }}" method="get" style="padding-bottom: 20px;">
                   <button class="btn btn-info btn-sm">
                     <i class="fas fa-pencil-alt">
                     </i>
@@ -76,34 +95,40 @@
                   </button>
                 </form>
                 <?php
-                $temp = 0;
-                foreach ($allproducts as $prod) {
-                  if ($prod['manu_id'] == $value['manu_id']) {
-                    $temp++;
-                  }
-                }
-                if ($temp != 0) { ?>
-                  <a class="btn btn-danger btn-sm" onclick="alert('Sản phầm còn tồn tại! Không thể xóa!')">
+                // $temp = 0;
+                // foreach ($allproducts as $prod) {
+                //   if ($prod['manu_id'] == $value['manu_id']) {
+                //     $temp++;
+                //   }
+                // }
+                // if ($temp != 0) { 
+                ?>
+                <!-- <a class="btn btn-danger btn-sm" onclick="alert('Sản phầm còn tồn tại! Không thể xóa!')">
                     <i class="fas fa-trash">
                     </i>
                     Delete
-                  </a>
-                <?php } else { ?>
-                  <form action="{{ url('/manufacturer/'.$value->manu_id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm">
-                      <i class="fas fa-trash">
-                      </i>
-                      Delete
-                    </button>
-                  </form>
-                <?php } ?>
+                  </a> -->
+                <?php
+                // } else { 
+                ?>
+                <form action="{{ url('/user/'.$value->id) }}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger btn-sm">
+                    <i class="fas fa-trash">
+                    </i>
+                    Delete
+                  </button>
+                </form>
+                <?php
+                // } 
+                ?>
               </td>
             </tr>
             @endforeach
           </tbody>
         </table>
+        {{ $allusers->links()}}
       </div>
       <!-- /.card-body -->
     </div>
